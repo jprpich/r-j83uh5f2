7b4b49b36861eea20bbcf2729471938102758c9c
Joshua <joshuaprpich@gmail.com>
2018-04-18 17:08:03 -0500

    resolve challenge

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+ 
+  constructor() {
+    super();
+    this.state = {
+      length: 0
+    }
+  }
+
+  handleChange(event){
+    this.setState({
+      length: event.target.value.length
+    });
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea onChange={this.handleChange.bind(this)} rows="3"></textarea>
+        <div className="counter">{this.state.length}</div>
       </div>
     );
   }
